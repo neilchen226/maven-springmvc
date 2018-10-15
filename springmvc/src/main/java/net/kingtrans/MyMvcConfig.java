08ea5c6ca963eb1add290dc6d5123009de770785
neilchen226 <157817985@qq.com>
2018-10-15 18:46:07 +0800

    首次提交

---
+++ springmvc/src/main/java/net/kingtrans/MyMvcConfig.java
@@ -0,0 +1,27 @@
+package net.kingtrans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc // 1
+@EnableScheduling
+@ComponentScan("net.kingtrans")
+public class MyMvcConfig extends WebMvcConfigurerAdapter {// 2
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/classes/views/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setViewClass(JstlView.class);
+		return viewResolver;
+	}
+
+}
